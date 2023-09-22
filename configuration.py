89f51f3ee52ab98e440749f025cff5dee335f889
Nadezhda <naduha1991@yandex.ru>
2023-09-22 21:16:03 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://913351d7-ddbf-45dd-b9bf-8d2163adcb3f.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ main.py
@@ -0,0 +1,6 @@
+def get_docs():
+    return requests.get(configuration.URL_SERVICE + configuration.DOC_PATH)
+
+response = get_docs()
+print(response.status_code)
+
